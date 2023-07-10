2dbdc289b22a3e1722d954d2181aac836f887428
mferreyra <mferreyra82@gmail.com>
2023-07-10 14:14:34 -0300

    Resuelto problema tamaño final

--- test.py
+++ test.py
@@ -1,7 +1,11 @@
 import pydicom as dycom
 import matplotlib.pyplot as plt
-#from PIL import Image
+from PIL import Image
+import os
+#import subprocess
+#import time
 
+#start = time.time()
 
 #print(im_np_array.shape)
 #im = Image.fromarray(im_np_array)
@@ -16,10 +20,34 @@ im = dycom.dcmread("./prueba2.dcm")
 im_np_array = im.pixel_array
 
 #rc('figure', figsize=(8.27,11.69))
-plt.figure(figsize=(8.27, 11.69), dpi=150)
-plt.imshow(im_np_array, cmap='gray_r', interpolation='nearest')
-plt.axis('off')
-plt.savefig("guardadoPrueba150.pdf", format='pdf', bbox_inches='tight', pad_inches=0)
+#plt.rcParams['figure.figsize'] = (8.27, 11.026)
+fig = plt.figure(figsize=(8.27, 11.69), dpi=100)
+#fig.subplots_adjust(left=0, right=0, top=0, bottom=0)
+ax = fig.add_subplot()
+ax.imshow(im_np_array, cmap='gray_r', interpolation='nearest')
+#plt.imshow(im_np_array, cmap='gray_r', interpolation='nearest')
+#print(im_np_array.shape)
+#plt.axis(False)
+ax.axis(False)
+
+#ax=plt.subplots_adjust
+
+#ax.text(1,1,"texto ArribaIzq", horizontalalignment='right', verticalalignment="top")
+#plt.legend("pasd")
+#plt.text(0,2048,"texto AbajoIzq")
+#plt.annotate(text="texto ArribaIzq", xy=[0,1], xycoords='figure fraction')
+#plt.text(1,1,"texto ArribaDer", horizontalalignment='right', verticalalignment="top")
+#plt.text(0,0,"texto AbajoDer", horizontalalignment='right', verticalalignment="bottom")
+plt.savefig("temp.png", format='png', orientation='portrait', bbox_inches='tight')
+#subprocess.check_call(["attrib","+H","temp.png"])
+imagen = Image.open("temp.png")
+imagen.save("fin.pdf", format="pdf")
+
+os.remove("temp.png")
+
+#end = time.time()
+#print(str(end-start))
+
 
 #plt.show()
 #plt.savefig("fname", dpi=None, facecolor='w', edgecolor='w', orientation='portrait', format=None, transparent=False, bbox_inches=None, pad_inches=0.1, metadata=None)

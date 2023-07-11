f4624e7a60f2bbf74fd571288f10999ecbb481bc
mferreyra <mferreyra82@gmail.com>
2023-07-11 11:03:15 -0300

    agregando log de errores

--- test.py
+++ test.py
@@ -5,55 +5,62 @@ import os
 import pathlib
 import pickle
 from tqdm.auto import tqdm
+from datetime import datetime
 import time
 
 start = time.time()
 
+#listar archivos en directorio
+#carpeta = pathlib.Path(r"C:\Users\mferreyra\Desktop\Kpacks Quilmes")
+carpeta = pathlib.Path(r"C:\Users\usuario\Documents\Trabajo\kpacks 28.06-30.06")
+listado_nuevo = set(carpeta.rglob("*.dcm"))
 
-carpeta = pathlib.Path(r"C:\Users\mferreyra\Desktop\Kpacks Quilmes")
-listado = set(carpeta.rglob("[0-9].*.dcm"))
+#cargar archivos ya procesados y hacer diff
+if os.path.exists("lista_procesados"):
+    with open ("lista_procesados", 'rb') as leer:
+        listado_previo = pickle.load(leer)
+else:
+    listado_previo = set()
+listado = listado_nuevo - listado_previo
 
+#leer archivos dicom
 for item in tqdm(listado, desc="Procesando imagenes", colour="green"):
-#leer dicom
-#im = dycom.dcmread("./mama.dcm")
-    #print(str(item))
-    im = dycom.dcmread(str(item))
-
-    im_np_array = im.pixel_array
+    try:
+        im = dycom.dcmread(str(item))
+        im_np_array = im.pixel_array
+    except Exception as e:
+        with open("errores.txt", "a+") as log:
+            log.write(f"{str(item)} -> {repr(e)} |{datetime.now().strftime('%D %H:%M:%S')}|\n")
+        continue
     #generar imagen
     plt.figure(figsize=(8.27, 11.69), dpi=100)
     plt.imshow(im_np_array, cmap='gray_r', interpolation='nearest')
     plt.axis(False)
     #guardar imagen a png (para buscar tamaño correcto en pdf)
     plt.savefig("temp.png", format='png', orientation='portrait', bbox_inches='tight')
-    #abrir y guardar en pdf en tamaño correcto con PIL
+    plt.close() #RuntimeWarning: Figures created through the pyplot interface are retained until explicitly closed and may consume too much memory.
+    #abrir para sobreescribir texto y guardar en pdf en tamaño correcto para A4 con PIL
     imagen = Image.open("temp.png")
-
     draw = ImageDraw.Draw(imagen)
-    font = ImageFont.truetype(r'C:\Users\System-Pc\Desktop\arial.ttf', 20)
-    text = "Paciente Pirulo \n Vino ayer\n Lo vio el otro medico"
+    font = ImageFont.truetype(r'C:\Users\System-Pc\Desktop\arial.ttf', 20) #Fuente?
+    text = "Paciente Pirulo \n Vino ayer\n Lo vio el otro medico" #ler atributos a escribir
     #Abajo izq, etc en coordenadas ralativas a imagen.size -> (w,h)
     draw.text((220, 400), text, font=font)    
     #imagen.show()
-
-    #print(imagen.size)
-
     imagen.save(fp="fin.pdf", format="pdf")
+
     #borrar archivo png temporal
     if os.path.exists("temp.png"):
         os.remove("temp.png")
 
-    #Agregar a listado de archivos procesados
-
-with open("lista", 'wb') as archivo:
+#guardar listado de archivos procesados, agregando a previos
+with open("lista_procesados", 'ab') as archivo:
     pickle.dump(listado, archivo, protocol=pickle.HIGHEST_PROTOCOL)
 
 end = time.time()
 print(str(end-start))
 
 #TODO
-#listar archivos dentro de carpetas/pacientes
-#diff para no volver a procesar mismos archivos / usar set()!
+#leer propiedades a guardar sobre imagen y escribir en cada esquina
 #csv para presentar archivos procesados?
-#leer propiedades a guardar sobre imagen
-#sobreescribir texto con datos en imagen png
+#generar archivo ejecutable

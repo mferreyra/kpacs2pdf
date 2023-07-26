ba5f812aa1ec72f786ff7012edb42c845eb236fe
mferreyra <mferreyra82@gmail.com>
2023-07-26 02:43:51 -0300

    Solucionado bug al leer dcm sin imagen

--- kpacs2pdf.py
+++ kpacs2pdf.py
@@ -10,10 +10,9 @@ import pydicom
 from tqdm.auto import tqdm
 
 
-def generar_imagen_temp(im, temp_file):
-    im_np_array = im.pixel_array
+def generar_imagen_temp(im_numpy_array, temp_file):
     plt.figure(figsize=(8.27, 11.69), dpi=150)  # para generar en tamaño de A4, con resolucion aceptable
-    plt.imshow(im_np_array, cmap="gray_r", interpolation="nearest")
+    plt.imshow(im_numpy_array, cmap="gray_r", interpolation="nearest")
     plt.axis(False)
     # guardar imagen a png (para buscar tamaño correcto en pdf)
     plt.savefig(temp_file, format="png", orientation="portrait", bbox_inches="tight")
@@ -51,7 +50,7 @@ def sobrescribir_imagen_temp(im, temp_file, nombre, fecha, hora):
     imagen.save(fp=temp_file, format="png")
 
 
-def generar_archivo_pdf(im, temp_file, nombre, fecha, hora):
+def generar_archivo_pdf(im, temp_file, nombre):
     # generar archivo pdf igual a formato nombre con bullzip: "Apellido- Nombre- - CR- form dia-mes-año S{num} I0"
     num = 0
     pdf_str_nombre = f"./pdf/{im.PatientID} {nombre}/{nombre.upper().replace(' ', '- ').replace(',', '- ')}"
@@ -74,8 +73,8 @@ def main():
     logging.config.dictConfig({"disable_existing_loggers": True, "version": 1})  # * Para apagar warning de img2pdf
 
     # listar archivos en directorio
-    # carpeta = Path(r"C:\Users\mferreyra\Desktop\Kpacks Quilmes")
-    carpeta = Path(r"C:\Users\usuario\Documents\Trabajo\kpacs 28.06-30.06")  # TODO cambiar a leer de config file
+    carpeta = Path(r"C:\Users\mferreyra\Desktop\Kpacks Quilmes")
+    # carpeta = Path(r"C:\Users\usuario\Documents\Trabajo\kpacs 28.06-30.06")  # TODO cambiar a leer de config file
     # carpeta = Path(r"C:\Users\usuario\source\kpacs2pdf\DICOM test")
     listado_nuevo = set(carpeta.rglob("*.dcm"))
 
@@ -97,6 +96,7 @@ def main():
     for item in tqdm(listado, desc="Procesando imagenes", colour="green", leave=True, position=0):
         try:
             im = pydicom.dcmread(str(item))
+            im_np_array = im.pixel_array
         except Exception as error:
             with open("kpacs2pdf_errores.txt", "a+") as log:
                 log.write(f"*|{datetime.now().strftime('%D %H:%M:%S')}| Archivo: {str(item)}\n    Error -> {repr(error)}\n")
@@ -105,14 +105,14 @@ def main():
         if (im.PatientID).startswith("1-"):  # * Saltear imagenes de MEVA para no procesarlas
             continue
 
-        temp_file = generar_imagen_temp(im, TEMP_FILE_NAME)  # TODO cambiar a Path
+        temp_file = generar_imagen_temp(im_np_array, TEMP_FILE_NAME)  # TODO cambiar a Path
         nombre = f"{im.PatientName}".replace("^", " ")
         fecha = f"{im.StudyDate[6:]}/{im.StudyDate[4:6]}/{im.StudyDate[0:4]}"
         hora = f"{im.AcquisitionTime[0:2]}:{im.AcquisitionTime[2:4]}:{im.AcquisitionTime[4:6]}"
         sobrescribir_imagen_temp(im, temp_file, nombre, fecha, hora)
         # if os.path.exists(temp_file):
         #    os.system('attrib +h kpacs2pdf_temp.temp') # ! Puede generar error de permiso de acceso si queda como archivo oculto
-        generar_archivo_pdf(im, temp_file, nombre, fecha, hora)
+        generar_archivo_pdf(im, temp_file, nombre)
 
     # borrar archivo png temporal
     if os.path.exists(TEMP_FILE_NAME):
@@ -133,6 +133,7 @@ if __name__ == "__main__":
 
 # TODO
 # Agregar directorio a procesar y guardar por consola? preguntando? argpars? os['ENV'] config.ini?
+# * carpeta = Path(r"C:\KPacs\Imagebox")
 # ? import tempfile para generar carpeta .tmp para archivo png?
 # ? Guardar uid de cada dicom dentro de meta de pdf? dentro de base de procesados?
 # CSV para presentar archivos procesados? probar reemplazar Pickle por SQLite?

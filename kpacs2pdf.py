57ca6d49d7ddf2913334e5d852eed59eef9d0d7e
mferreyra <mferreyra82@gmail.com>
2023-07-30 15:31:18 -0300

    Agregado config file

--- kpacs2pdf.py
+++ kpacs2pdf.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from configparser import ConfigParser
 import img2pdf
 import logging.config
 import matplotlib.pyplot as plt
@@ -35,10 +36,10 @@ def sobrescribir_imagen_temp(im, TEMP_FILE, nombre_paciente, fecha, hora):
     # texto a escribir en cada una
     text_sup_izq = f"{nombre_paciente}\n{im.PatientSex}\nID: {im.PatientID}"
     text_sup_der_1 = f"{im.InstitutionName}\n"
-    text_sup_der_2 = f"\n Ref: {im.ReferringPhysicianName} / Perf:"  # ? Si im.ReferringPhysicianName no esta vacio, se puede ir de la image?
+    text_sup_der_2 = f"\n Ref: {im.ReferringPhysicianName} / Perf:"  # *Si im.ReferringPhysicianName no vacio,sale de la imagen?
     text_sup_der_3 = f"\n\nStudy date: {fecha}\nStudy time: {hora}"
     text_inf_izq = (f"W{im.WindowWidth}  /  C{im.WindowCenter}\n \n S-Value: {im.Sensitivity}")
-    text_inf_der = f"{im.BodyPartExamined }\nPosition: {im.ViewPosition}\n{im.InstanceNumber} IMA {im.SeriesNumber}\nZoom factor: x0.99"  # Zoom harcoded!
+    text_inf_der = f"{im.BodyPartExamined }\nPosition: {im.ViewPosition}\n{im.InstanceNumber} IMA {im.SeriesNumber}\nZoom factor: x0.99"  # *Zoom harcoded!
     draw.text(sup_izq, text_sup_izq, font=font, fill="white", stroke_width=1, stroke_fill="black")
     draw.text(sup_der_1, text_sup_der_1, font=font, fill="white", stroke_width=1, stroke_fill="black")
     draw.text(sup_der_2, text_sup_der_2, font=font, fill="white", stroke_width=1, stroke_fill="black")
@@ -50,13 +51,13 @@ def sobrescribir_imagen_temp(im, TEMP_FILE, nombre_paciente, fecha, hora):
 
 
 def generar_archivo_pdf(im, TEMP_FILE, nombre_paciente, fecha, CARPETAS_PDF):
-    # generar archivo pdf igual a formato nombre con bullzip: "Apellido- Nombre- - CR from dia-mes-año S{num} I0"
+    # generar pdf con  mismo nombre que bullzip: "Apellido- Nombre- - CR from dia-mes-año S{num} I0"
     num = 0
     nombre_carpeta = f"{im.PatientID} {nombre_paciente}"
     nombre_upper = f"{nombre_paciente.upper().replace(' ', '- ').replace(',', '- ')}"
     while os.path.exists(f"{CARPETAS_PDF}/{nombre_carpeta}/{nombre_upper}- - CR from {fecha} S{num} I0.pdf"):
-        num += 1  # ! Puede ocurrir que duplique imágenes si vuelvo a procesar la misma, ya que le asignará un num diferente (Si borro lista procesados)
-        if num > 20:
+        num += 1  # ! Puede que duplique imágenes si vuelvo a procesar la misma (Si borro lista procesados)
+        if num > 50:  # numero arbitrario de máxima cantidad de imagenes por paciente?
             break
     nombre_pdf = f"{nombre_upper}- - CR from {fecha} S{num} I0"
     if not os.path.exists(f"{CARPETAS_PDF}/{nombre_carpeta}"):
@@ -69,31 +70,43 @@ def generar_archivo_pdf(im, TEMP_FILE, nombre_paciente, fecha, CARPETAS_PDF):
 
 
 def main():
-    logging.config.dictConfig({"disable_existing_loggers": True, "version": 1})  # * Para apagar warning de img2pdf
-    LISTA_PROCESADOS = Path("kpacs2pdf_lista_procesados").absolute()
-    TEMP_FILE = Path("kpacs2pdf_temp.temp").absolute()
-    ARCHIVO_ERRORES = Path("kpacs2pdf_errores.txt").absolute()
-    CARPETAS_PDF = Path("./pdf").absolute()
-    # RUTA_DICOM = 
+    logging.config.dictConfig({"disable_existing_loggers": True, "version": 1})  # *Para apagar warning de img2pdf
+    DEFAULT_PATH = Path.cwd()/"kpacs2pdf.ini"
+    DEFAULT_DICOM_DIR = "C:/Users/usuario/Documents/Trabajo/kpacs 28.06-30.06"  # *"C:/KPacs/Imagebox"
+    DEFAULT_PDF_DIR = Path.cwd()/"pdf"
 
+    config = ConfigParser()
+    try:
+        config.read(DEFAULT_PATH, encoding='UTF-8')
+    except:
+        with open(DEFAULT_PATH, 'x', encoding='UTF-8') as config_file:
+            config_file.write(
+            f'[dicom (Kpacks/Imagebox)]\n'
+            f'DICOM_dir = {DEFAULT_DICOM_DIR}\n\n'
+            f'[pdf (Pacientes)]\n'
+            f'PDF_dir = {DEFAULT_PDF_DIR}\n')
+    finally:
+        config.read(DEFAULT_PATH, encoding='UTF-8')
+    # rutas
+    LISTA_PROCESADOS = Path.cwd()/"kpacs2pdf_lista_procesados"
+    TEMP_FILE = Path.cwd()/"kpacs2pdf_temp.temp"
+    ARCHIVO_ERRORES = Path.cwd()/"kpacs2pdf_errores.txt"
+    CARPETAS_PDF = Path(config.get('pdf (Pacientes)', 'PDF_dir').strip('\"'))
+    CARPETA_IMAGEBOX = Path(config.get('dicom (Kpacks/Imagebox)', 'DICOM_dir').strip('\"'))
     # listar archivos en directorio
-    # carpeta = Path(r"C:/Users/mferreyra/Desktop/Kpacks Quilmes")
-    carpeta = Path("C:/Users/usuario/Documents/Trabajo/kpacs 28.06-30.06")  # TODO cambiar a leer de config file
-    # carpeta = Path(r"C:/Users/usuario/source/kpacs2pdf/DICOM test")
-    listado_nuevo = set(carpeta.rglob("*.dcm"))
+    listado_dcm_nuevo = set(CARPETA_IMAGEBOX.rglob("*.dcm"))
     # cargar archivos ya procesados y hacer diff
     if os.path.exists(LISTA_PROCESADOS):  # TODO cambiar a base SQLite
         with open(LISTA_PROCESADOS, "rb") as file:
-            listado_procesados = pickle.load(file)
+            listado_dcm_procesados = pickle.load(file)
     else:
-        listado_procesados = set()
-    listado = listado_nuevo - listado_procesados
+        listado_dcm_procesados = set() # *Rutas sin duplicados
+    listado_dcm = listado_dcm_nuevo - listado_dcm_procesados
     # crear carpeta para guardar pdf si no existe (evit error en img2pdf with open())
-    if not os.path.exists(CARPETAS_PDF):  # TODO leer de config file
-        os.makedirs(CARPETAS_PDF)
-
+    if not os.path.exists(CARPETAS_PDF):
+        os.mkdir(CARPETAS_PDF)
     # leer archivos dicom
-    for item in tqdm(listado, desc="Procesando imagenes", colour="green", leave=True, position=0):
+    for item in tqdm(listado_dcm, desc="Procesando imagenes", colour="green", leave=True, position=0):
         try:
             im = pydicom.dcmread(str(item))
             im_np_array = im.pixel_array
@@ -103,11 +116,11 @@ def main():
             continue
         if (im.PatientID).startswith("1-"):  # * Saltear imagenes de MEVA para no procesarlas
             continue
-
+        # procesados
         nombre_paciente = f"{im.PatientName}".replace("^", " ")
         fecha = f"{im.StudyDate[6:]}-{im.StudyDate[4:6]}-{im.StudyDate[0:4]}"
         hora = f"{im.AcquisitionTime[0:2]}:{im.AcquisitionTime[2:4]}:{im.AcquisitionTime[4:6]}"
-        generar_imagen_temp(im_np_array, TEMP_FILE)  # TODO cambiar a Path
+        generar_imagen_temp(im_np_array, TEMP_FILE)
         sobrescribir_imagen_temp(im, TEMP_FILE, nombre_paciente, fecha, hora)
         generar_archivo_pdf(im, TEMP_FILE, nombre_paciente, fecha, CARPETAS_PDF)
 
@@ -116,23 +129,16 @@ def main():
         os.remove(TEMP_FILE)
     # guardar listado de archivos procesados, agregando a previos
     with open(LISTA_PROCESADOS, "wb") as archivo:
-        pickle.dump(listado_nuevo, archivo, protocol=pickle.HIGHEST_PROTOCOL)
+        pickle.dump(listado_dcm_nuevo, archivo, protocol=pickle.HIGHEST_PROTOCOL)  # TODO ver SQLite
 
 
 if __name__ == "__main__":
     main()
 
-# * Para compilar archivo con pyinstaller sin errores
-# * pyinstaller --onefile -F --hiddenimport=pydicom.encoders.gdcm --hiddenimport=pydicom.encoders.pylibjpeg
-# * --upx-dir=C:/Users/usuario/source/kpacs2pdf/upx-4.0.2-win64 --icon=C:/Users/usuario/source/kpacs2pdf/splash.ico --clean kpacs2pdf.py
-# * agregar --upx-dir "CARPETA" para reducir tamaño de .exe generado en carpeta dist
 
 # TODO
-# Agregar directorio a procesar y guardar por consola? preguntando? argpars? os['ENV'] config.ini?
-# * carpeta = Path("C:/KPacs/Imagebox")
-# ? import tempfile para generar carpeta .tmp para archivo png?
-# ? Guardar uid de cada dicom dentro de meta de pdf? dentro de base de procesados?
-# CSV para presentar archivos procesados? probar reemplazar Pickle por SQLite?
+# ? Guardar uid de cada dicom dentro de meta de pdf? en db de procesados?
+# reemplazar Pickle por SQLite?
 # Modificar posicion texto segun DPI imagen y segun longitud string? #im.size, get textbox size, etc.
 # Usar Mypy
-# Generar tests
+# ? Generar tests
